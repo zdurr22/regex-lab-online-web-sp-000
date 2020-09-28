a527e4edb232c4f06d4158675fe83a5d03f8d04e
Zachary Durr <zdurr22@gmail.com>
2020-09-28 01:04:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,8 @@
 def starts_with_a_vowel?(word)
-vowel_words = starts_with_a_vowel?(word).scan(/\b[aeiou]/)
-  if vowel_words != nil
-  true
+  if word.match(/\b[aeiou]/) == nil
+    return false
   else
-  false
+    return true
   end
 end
 
@@ -12,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
